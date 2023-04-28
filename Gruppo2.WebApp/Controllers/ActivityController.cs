7b2a937781d540e5d287503f174d0f8463748645
valentmatteo <matteo.valent@stud.itsaltoadriatico.it>
2023-04-28 17:05:48 +0200

    add controllers

---
+++ Gruppo2.WebApp/Controllers/ActivityController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Gruppo2.WebApp.Models;
+using Gruppo2.WebApp.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Gruppo2.WebApp.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class ActivityController : ControllerBase
+    {
+        private readonly WebAppContex _context;
+
+        private readonly IMapper _mapper;
+        public ActivityController(WebAppContex context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<bool>> GetActivityAsync()
+        {
+            List<Models.Activity> activitys = new List<Models.Activity>();
+            activitys = await _context.Activity.ToListAsync();
+            Console.WriteLine(activitys);
+            return true;
+        }
+    }
+}
---
+++ Gruppo2.WebApp/Controllers/DeviceController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Gruppo2.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Gruppo2.WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DeviceController : ControllerBase
+    {
+        private readonly WebAppContex _context;
+
+        private readonly IMapper _mapper;
+        public DeviceController(WebAppContex context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<bool>> GetDeviceAsync()
+        {
+            List<Device> devices = new List<Device>();
+            devices = await _context.Device.ToListAsync();
+            Console.WriteLine(devices);
+            return true;
+        }
+    }
+}
