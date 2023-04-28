2966cf12e2cc9798a381a18568c917019a2225de
valentmatteo <matteo.valent@stud.itsaltoadriatico.it>
2023-04-28 17:44:52 +0200

    add Username on User and create Login su AuthenticateController

--- Gruppo2.WebApp/Controllers/AuthenticateController.cs
+++ Gruppo2.WebApp/Controllers/AuthenticateController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Gruppo2.WebApp.Models;
+using Gruppo2.WebApp.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
@@ -17,10 +18,19 @@ namespace Gruppo2.WebApp.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet("Login/{username}/{password}")]
+        [HttpGet("Login/{username}/{password}")]//prova login, da cambiare con auth0
         public async Task<ActionResult<bool>> Login(string username, string password)
         {
-            
+            User user = new User();
+            user = await _context.User.Where(x => x.Username == username && x.Password == password).FirstAsync();
+            if(user.Id == null) //non trovo nessun user con quelle credenziali
+            {
+                return NoContent();
+            }
+            UserDto userDto = new UserDto();
+            _mapper.Map(user, userDto);
+
+            return Ok(userDto);
         }
     }
 }
