4fc69ff7c9eb2c6060d8519b15545bb5dbd0ea12
simonvianello <simon.vianello@stud.itsaltoadriatico.it>
2023-04-28 17:29:30 +0200

    add authenticateController

---
+++ Gruppo2.WebApp/Controllers/AuthenticateController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Gruppo2.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Gruppo2.WebApp.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        private readonly WebAppContex _context;
+
+        private readonly IMapper _mapper;
+        public AuthenticateController(WebAppContex context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("Login/{username}/{password}")]
+        public async Task<ActionResult<bool>> Login(string username, string password)
+        {
+            
+        }
+    }
+}
