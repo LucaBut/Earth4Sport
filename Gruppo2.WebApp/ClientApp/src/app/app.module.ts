8a5b868fe6baf6c0412c8bf3a9c89f1b68d34004
LucaBut <luca.but@stud.itsaltoadriatico.it>
2023-05-04 09:22:57 +0200

    Removed default angular component

--- Gruppo2.WebApp/ClientApp/src/app/app.module.ts
+++ Gruppo2.WebApp/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { DxDataGridModule } from 'devextreme-angular';
 
 @NgModule({
@@ -16,8 +14,6 @@ import { DxDataGridModule } from 'devextreme-angular';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,8 +22,7 @@ import { DxDataGridModule } from 'devextreme-angular';
     DxDataGridModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      // { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [],
