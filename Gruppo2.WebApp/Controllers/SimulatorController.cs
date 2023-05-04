ff45028537fa96558534e779a8014dc2ab49e156
valentmatteo <matteo.valent@stud.itsaltoadriatico.it>
2023-05-04 12:57:09 +0200

    add simulator service and controller

---
+++ Gruppo2.WebApp/Controllers/SimulatorController.cs
@@ -0,0 +1,25 @@
+﻿using Gruppo2.WebApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppo2.WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimulatorController : ControllerBase
+    {
+        private readonly SimulatorService _simulator;
+        public SimulatorController(SimulatorService simulator)
+        {
+            _simulator = simulator;
+        }
+
+
+        [HttpGet("StartOperation")]
+        public async Task StartOperation()
+        {
+            _simulator.Start();
+             throw new Exception();
+        }
+    }
+}
