86b635a936e4ce771a6811d7ca07c2696878f796
simonvianello <simon.vianello@stud.itsaltoadriatico.it>
2023-06-08 12:02:09 +0200

    fix vari per nuova appadmin

---
+++ Gruppo2.AdminApp/Controllers/NotificationErrorController.cs
@@ -0,0 +1,33 @@
+﻿using Gruppo2.AdminApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Gruppo2.AdminApp.Models;
+
+namespace Gruppo2.AdminApp.Controllers
+{       
+    [ApiController]
+    [Route("[controller]")]
+    public class NotificationErrorController : ControllerBase
+    {
+
+        private readonly AdminDBContext _context;
+        private readonly IMapper _mapper;
+        public NotificationErrorController(AdminDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("GetNotificationErrors")]
+        public async Task<ActionResult<IEnumerable<NotificationError>>> GetNotificationErrors()
+        {
+            List<NotificationError> errors = new List<NotificationError>();
+            errors = await _context.NotificationError.ToListAsync();
+
+            //aggiungere le chiamate al db esterno (link)
+        }
+    }
+}
---
+++ Gruppo2.AdminApp/Services/AdminDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Gruppo2.AdminApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Gruppo2.AdminApp.Services
+
+{
+    public class AdminDBContext : DbContext
+    {
+        public AdminDBContext(DbContextOptions<AdminDBContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<NotificationError> NotificationError { get; set; }
+    }
+}
