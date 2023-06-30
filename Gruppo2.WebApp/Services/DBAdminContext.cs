b320f33dad67e83afacdc19e772a7bb92d1f8d94
simonvianello <simon.vianello@stud.itsaltoadriatico.it>
2023-06-30 09:34:20 +0200

    ciao2

---
+++ Gruppo2.WebApp/Services/DBAdminContext.cs
@@ -0,0 +1,14 @@
+﻿using Gruppo2.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Gruppo2.WebApp.Services
+{
+    public class DBAdminContext : DbContext
+    {
+        public DBAdminContext(DbContextOptions<DBAdminContext> optionsAdmin) : base(optionsAdmin)
+        {}
+
+        public virtual DbSet<NotificationError> NotificationError{ get; set; }
+    }
+}
